0a5f4a3bf9876dc41162774ef0f553f7e11efacc
hajiiiin <101108440+hajiiiin@users.noreply.github.com>
2025-11-06 21:45:26 +0900

    fix: 무한 렌더링 오류 해결

--- src/pages/SpherePageWeb.jsx
+++ src/pages/SpherePageWeb.jsx
@@ -46,7 +46,9 @@ export default function SpherePageWeb() {
     [rawData]
   );
 
-  setStockData(sphereCoords);
+  useEffect(() => {
+    setStockData(sphereCoords);
+  }, [sphereCoords]);
 
   return (
     <div
